48a4bf5dd63fb7fd20d10d20b6ce6c20da50c2ca
Phill <fotofill.holland@googlemail.com>
2024-04-02 17:01:10 +0100

    3 epochs test

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -89,7 +89,7 @@ organisation::parameters get_parameters()
     
     parameters.input.push_back(epoch1);
     parameters.input.push_back(epoch2);
-    //parameters.input.push_back(epoch3);
+    parameters.input.push_back(epoch3);
     //parameters.input.push_back(epoch4);
     
     organisation::dictionary words;
